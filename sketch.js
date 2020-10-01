7d346668cc5d56f71b892b8ac1fd04aa54030b23
Tanveeradesh <tanveeradesh@gmail.com>
2020-10-01 07:32:07 +0530

    constrained body demo1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint=Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var constrainedlog;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+constrainedlog= new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -36,6 +36,14 @@ function setup(){
 
     bird = new Bird(100,100);
 
+var options={
+bodyA:bird.body,
+bodyB:constrainedlog.body,
+stiffness:0.04,
+length:10
+}
+var chain=Constraint.create(options);
+World.add(world,chain);
 }
 
 function draw(){
@@ -61,4 +69,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedlog.display();
+    strokeWeight(4);
+    line(bird.body.position.x,bird.body.position.y,constrainedlog.body.position.x,constrainedlog.body.position.y);
 }
